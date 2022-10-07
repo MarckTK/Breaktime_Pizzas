656ff6af299136fd1fe930c45540c8c8199aed77
MarckTK <perezmatosmarco14@gmail.com>
2022-10-07 11:54:23 -0500

    Nosotros y Ordenar

--- js/script.js
+++ js/script.js
@@ -36,3 +36,18 @@ window.onscroll = () =>{
     myOrders.classList.remove('active');
     cart.classList.remove('active');
 };
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
