911fc19e331309ed3206b86d2c8367a7fe7c6598
MarckTK <perezmatosmarco14@gmail.com>
2022-11-10 20:11:57 -0500

    Ajuste

--- breaktime_bd.sql
+++ breaktime_bd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-10-2022 a las 03:28:08
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.12
+-- Tiempo de generación: 11-11-2022 a las 01:44:45
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,7 +38,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `name`, `password`) VALUES
-(1, 'admin', '8cb2237d0679ca88db6464eac60da96345513964');
+(1, 'admin', '8cb2237d0679ca88db6464eac60da96345513964'),
+(7, 'Perez', 'acca16d632c5e2b5fd3a58787e632e0ba4446cfc');
 
 -- --------------------------------------------------------
 
@@ -75,6 +76,16 @@ CREATE TABLE `orders` (
   `payment_status` varchar(20) NOT NULL DEFAULT 'Pendiente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(1, 1, 'Freddy Niño', '951243579', 'En efectivo', 'Jr. Rio Chico, Calle 05', 'Clásica (2), Cherry (1), FullQueso (4)', 36, '2022-10-06', 'Pendiente'),
+(8, 3, 'Marco', '948512301', 'En efectivo', 'Av. Eduardo de Habich 599, San Martín de Porres, Ovalo Panamericana - 15103', '-Clásica (S/8 x 2) ', 16, '2022-10-31', 'Pendiente'),
+(9, 3, 'Maco :)', '996034101', 'En efectivo', 'Av. Eduardo de Habich 599, San Martín de Porres, Ovalo Panamericana - 15102', '-Champiñones (S/16 x 4) ', 64, '2022-11-06', 'Pendiente'),
+(10, 3, 'Andres', '948421256', 'Tarjeta de Crédito', 'Av. Carrión, Ovalo Panamericana - 15102', '-Cherry (S/8 x 3) ', 24, '2022-11-07', 'Pendiente');
+
 -- --------------------------------------------------------
 
 --
@@ -88,6 +99,15 @@ CREATE TABLE `products` (
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `image`) VALUES
+(5, 'Clásica', 8, 'clasica-1 PNG.png'),
+(6, 'Champiñones', 16, 'champiñones-2 PNG.png'),
+(7, 'Cherry', 8, 'cherry-3 PNG.png');
+
 -- --------------------------------------------------------
 
 --
@@ -100,6 +120,13 @@ CREATE TABLE `user` (
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password`) VALUES
+(3, 'Marco', 'fivecol462@herrain.com', '3fcfc1f7f34e78a937e81171ba51dc39538db993');
 
 --
 -- Índices para tablas volcadas
@@ -143,31 +170,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
